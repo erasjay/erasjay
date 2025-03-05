1281e25d8b278c37842ee094a0a85fa8b247125f
erasmus <erasossis@gmail.com>
2025-03-05 23:06:11 +0000

    Initial Commit

---
+++ TrustPact/TrustPactApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrustPactApp.swift
+//  TrustPact
+//
+//  Created by Erasmus Austin  on 05/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct TrustPactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TrustPactTests/TrustPactTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrustPactTests.swift
+//  TrustPactTests
+//
+//  Created by Erasmus Austin  on 05/03/2025.
+//
+
+import Testing
+@testable import TrustPact
+
+struct TrustPactTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
